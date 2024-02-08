03909e42e06b69ecbd7de7d57740de9508eac7a6
James Richardson <76536315+Icyfiremario@users.noreply.github.com>
2024-02-08 13:48:33 -0700

    Added plog

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
+//Local
 #include "Game.h"
 
+//STD
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
+
+//Plog
+#include <plog/Log.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+
 int main()
 {
+
+	plog::init(plog::debug, "./logs/runtime_log.log");
+
+	PLOGI << "Starting engine";
+
 	Game game{};
 
 	try 
